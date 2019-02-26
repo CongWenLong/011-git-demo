0fb8a12e9675a56582685364916d4ea9ac17e5b0
bj <bj@126.com>
2019-02-26 17:51:58 +0800

    ceshi

---
+++ src/main/java/com/cwl/maven/Demo.java
@@ -0,0 +1,15 @@
+package com.cwl.maven;
+
+/**
+ * ClassName:Demo
+ * Package:com.cwl.maven
+ * Description:
+ *
+ * @date:2019-2-26 17:32
+ * @author:13641371412
+ */
+public class Demo {
+    public static void main(String[] args) {
+        System.out.println("你好啊");
+    }
+}
